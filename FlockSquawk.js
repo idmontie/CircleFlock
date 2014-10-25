59f62fe39dd23add5350f1f07dcc91d01a2dc0e7
Ivan Montiel <idmontie@asu.edu>
2014-10-24 23:39:28 -0700

    twitter bearer

--- FlockSquawk.js
+++ FlockSquawk.js
@@ -50,6 +50,39 @@ if (Meteor.isServer) {
     var twitterConfig = JSON.parse( textBlob )
 
     var credentials = twitterConfig.consumerKey + ":" + twitterConfig.consumerSecret
+
+    var base64Creds = Base64.encode( credentials )
+
+
+    console.log(base64Creds)
+    var bearer = HTTP.call(
+      'POST', 
+      'https://api.twitter.com/oauth2/token',
+      {
+        'headers' : {
+          'Authorization' : 'Basic ' + base64Creds,
+          'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8'
+        },
+        'content' : 'grant_type=client_credentials'
+      }
+      )
+
+    console.log( bearer )
+
+    // TODO nonce?
+    var nonce = '5485d7184c2cb43f96d34b9ad0ba69a8'
+    // TODO oauth signature?
+    var siggy = 'dGn6Xrxe3Por85dyklMGff9ngRY%3D' 
+
+    var oauth = 'OAuth oauth_consumer_key="'
+    oauth += twitterConfig.consumerKey
+    oauth += '", oauth_nonce="'
+    oauth += nonce
+    oauth += '", oauth_signature="'
+    oauth += siggy
+    oauth += '", oauth_signature_method="HMAC-SHA1", oauth_timestamp="1414215527", oauth_token="'
+    oauth += twitterConfig.accessToken
+    oauth +='", oauth_version="1.0"'
     
   });
 }
